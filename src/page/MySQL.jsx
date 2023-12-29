682d3089df62b8e93c4c8087dc14bfa150127692
unknown <rifkymuhamad0022@gmail.com>
2023-12-29 10:10:26 +0700

    modify MySQL.jsx

--- src/page/MySQL.jsx
+++ src/page/MySQL.jsx
@@ -1,13 +1,15 @@
 const MySQL = () => {
-    const isi = "mysql cheat sheet\n    Database \n    show databases; \n    create database nama_database;\n    use nama_database; \n    drop database nama_database; \n\n    Tipe Data \n    Integer \n        Tinyint \n\t        Smallint \n\t        Mediumint \n\t        Int \n\t        Bigint \n    Floating Point \n\t        Float \n\t        Double \n";
+  const isi =
+    "mysql cheat sheet\n    Database \n    show databases; \n    create database nama_database;\n    use nama_database; \n    drop database nama_database; \n\n    Tipe Data \n    Integer \n        Tinyint \n\t        Smallint \n\t        Mediumint \n\t        Int \n\t        Bigint \n    Floating Point \n\t        Float \n\t        Double \n";
   return (
     <div>
-        mysql cheat sheet
+      <div>mysql cheat sheet</div>
+      <div>
         Database
-
-        show databases;
+        <div>show databases;</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MySQL
+export default MySQL;
