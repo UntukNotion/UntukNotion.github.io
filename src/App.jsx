c7852e0e18d4214dbe8de53d84ce6441a15d0d88
unknown <rifkymuhamad0022@gmail.com>
2023-12-29 10:04:22 +0700

    Create MySQL.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./page/Home";
+import MySQL from "./page/MySQL";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mysql-cheat-sheet" element={<MySQL />} />
       </Routes>
     </HashRouter>
   );
